28d669e0ccc7f0d5e051caf92d7c1fbd826d2a1e
XLoganxmenX <loganxmen97@gmail.com>
2023-08-23 21:15:36 +0900

    Complete task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,44 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String text = "";
+
+        try (FileInputStream fileInputStream = new FileInputStream(file);){
+            int ch;
+            while ((ch=fileInputStream.read()) != -1) {
+               text += (char) ch;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] stringList = text.split("[\\r\\n]+");
+
+        String name = extractValueFromString(stringList, "Name:");
+        int age = Integer.parseInt(extractValueFromString(stringList, "Age:"));
+        String email = extractValueFromString(stringList, "Email:");
+        Long phone = Long.parseLong(extractValueFromString(stringList, "Phone:"));
+
+        return new Profile(name, age, email, phone);
     }
+
+    private String extractValueFromString(String[] stringList, String keyword) {
+        for (String s : stringList) {
+            if (s.startsWith(keyword)) {
+                return s.substring(keyword.length()).trim();
+            }
+        }
+        return "";
+    }
+
 }
